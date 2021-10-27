d055e931918cd056d2f77f67b9c8b2344e593940
binxu <binxu.wang@wustl.edu>
2021-10-26 23:19:39 -0400

    Add implementation for issubset

--- src/interval.jl
+++ src/interval.jl
@@ -8,3 +8,5 @@ Base.minimum(iv::Interval) = iv.min
 Base.maximum(iv::Interval) = iv.max
 
 Base.in(x::Real, iv::Interval) = (x <= iv.max) & (x >= iv.min)
+
+Base.issubset(iv1::Interval, iv2::Interval) = (iv1.max <= iv2.max) & (iv1.min >= iv2.min)
