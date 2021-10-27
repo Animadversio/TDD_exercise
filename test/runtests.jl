6fcfc2350c844832d2fe37d7d8bfeca6a39348df
binxu <binxu.wang@wustl.edu>
2021-10-26 21:14:49 -0400

    add test for getting neighbor

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,13 @@ using Test
 
 @testset "TDD.jl" begin
     # Write your tests here.
+    graph = [
+            [2, 3],
+            [1], # list of nodes that can be reached from node 2
+            [1], # ", node 3
+            [5], # ", node 4
+            [4]] # ", node 5
+    @test TDD.get_neighbors(graph, 1) == [1,2,3]
+    @test TDD.get_neighbors(graph, 2) == [1,2]
+    @test TDD.get_neighbors(graph, 5) == [4,5]
 end
