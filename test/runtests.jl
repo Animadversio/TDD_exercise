2e7db527313e1ca34332316894d094360d7c8530
binxu <binxu.wang@wustl.edu>
2021-10-26 21:29:03 -0400

    add more test and parametric ones

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,19 @@ using Test
             [1], # ", node 3
             [5], # ", node 4
             [4]] # ", node 5
+    dir_graph = [
+        [2],
+        [3,5], # list of nodes that can be reached from node 2
+        [4], # ", node 3
+        [1,2], # ", node 4
+        [4]] # ", node 5
     @test TDD.get_neighbors(graph, 1) == [1,2,3]
     @test TDD.get_neighbors(graph, 2) == [1,2]
     @test TDD.get_neighbors(graph, 5) == [4,5]
+    @test TDD.get_neighbors(graph, 5) == [4,5]
+    @test TDD.get_neighbors(dir_graph, 2) == [2,3,5]
+    @test TDD.get_neighbors(dir_graph, 5) == [4,5]
+    for i in 1:length(dir_graph)
+        @test i in TDD.get_neighbors(dir_graph, i)
+    end
 end
