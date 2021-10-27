132bc65a8b4ff656271a9612597726f69e54ecf6
binxu <binxu.wang@wustl.edu>
2021-10-26 23:11:34 -0400

    add tests for in

--- test/interval.jl
+++ test/interval.jl
@@ -1,7 +1,11 @@
 iv = Interval(0,1)
 @test minimum(iv) == 0
 @test maximum(iv) == 1
+@test 0.5 in iv
+@test !(-0.5 in iv)
 
 iv = Interval(-0.2,1)
 @test minimum(iv) == -0.2
 @test maximum(iv) == 1
+@test -0.2 in iv
+@test !(1.5 in iv)
